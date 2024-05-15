7b5b3cf49626a2b36270897b481d5134f72d10ef
Sharoun Madoya <madoyasharoun@gmail.com>
2024-05-15 09:43:20 +0300

    :construction_worker: added interphase

--- client/src/components/TicTac.js
+++ client/src/components/TicTac.js
@@ -3,7 +3,27 @@ import './TicTac.css'
 
 const TicTac= () =>{
     return(
-        <div></div>
+        <div className='container'>
+            <h1>Tic Tac Toe</h1>
+            <div className='board'>
+                <div className='row1'>
+                    <div className='boxes'></div>
+                    <div className='boxes'></div>
+                    <div className='boxes'></div>
+                </div>
+                <div className='row2'>
+                    <div className='boxes'></div>
+                    <div className='boxes'></div>
+                    <div className='boxes'></div>
+                </div>
+                <div className='row3'>
+                    <div className='boxes'></div>
+                    <div className='boxes'></div>
+                    <div className='boxes'></div>
+                </div>
+            </div>
+            <button className='btn'>Refresh</button>
+        </div>
     )
 }
 
