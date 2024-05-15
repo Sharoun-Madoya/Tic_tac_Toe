ecc836a4f2d0aee1a7d83117c892156bb50d75a8
Sharoun Madoya <madoyasharoun@gmail.com>
2024-05-15 10:08:09 +0300

    :construction: progressing

--- client/src/components/TicTac.js
+++ client/src/components/TicTac.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TicTac.css'
 
+let data = ["","","","","","","","",""]
 const TicTac= () =>{
+    let [count, setCount]= useState(0)
+    let [Lock, setLock]= useState(false)
+    const toogle = () =>{
+        if (lock){
+            return 0;
+        }
+        if (count%2 ===0){
+            e.target.innerHTML= `<img src='${}'>`;
+            data[num]="x";
+            setCount(++count);
+        }
+        else{
+            e.target.innerHTML=`<img src='${}'>`;
+            data[num]= "o";
+            setCount(++count);
+        }
+    }
     return(
         <div className='container'>
             <h1>Tic Tac Toe</h1>
             <div className='board'>
                 <div className='row1'>
-                    <div className='boxes'></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 0)}}></div>
                     <div className='boxes'></div>
                     <div className='boxes'></div>
                 </div>
