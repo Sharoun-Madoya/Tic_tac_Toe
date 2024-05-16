d2fb2a9381f9ac42fdf38109ef0cb00e9e0e9d43
Sharoun Madoya <madoyasharoun@gmail.com>
2024-05-16 12:30:14 +0300

    :green_heart: good run

--- client/src/components/TicTac.js
+++ client/src/components/TicTac.js
@@ -7,8 +7,8 @@ import circle_icon from "../assets/O1 tictac.png"
 let data = ["","","","","","","","",""]
 const TicTac= () =>{
     let [count, setCount]= useState(0)
-    let [Lock, setLock]= useState(false)
-    const toogle = (e, lock, num) =>{
+    let [lock, setLock]= useState(false)
+    const toogle = (e, num) =>{
         if (lock){
             return 0;
         }
@@ -29,18 +29,18 @@ const TicTac= () =>{
             <div className='board'>
                 <div className='row1'>
                     <div className='boxes' onClick={(e)=>{toogle(e, 0)}}></div>
-                    <div className='boxes'></div>
-                    <div className='boxes'></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 1)}}></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 2)}}></div>
                 </div>
                 <div className='row2'>
-                    <div className='boxes'></div>
-                    <div className='boxes'></div>
-                    <div className='boxes'></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 3)}}></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 4)}}></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 5)}}></div>
                 </div>
                 <div className='row3'>
-                    <div className='boxes'></div>
-                    <div className='boxes'></div>
-                    <div className='boxes'></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 6)}}></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 7)}}></div>
+                    <div className='boxes' onClick={(e)=>{toogle(e, 8)}}></div>
                 </div>
             </div>
             <button className='btn'>Refresh</button>
