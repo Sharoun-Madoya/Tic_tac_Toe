dc49de84e8415d20621a338570141dd358d3e73e
Sharoun Madoya <madoyasharoun@gmail.com>
2024-05-22 20:07:07 +0300

    :bug: unable to clear reset functionality

--- client/src/components/TicTac.js
+++ client/src/components/TicTac.js
@@ -9,6 +9,18 @@ const TicTac= () =>{
     let [count, setCount]= useState(0)
     let [lock, setLock]= useState(false)
     let titleRef = useRef(null)
+    let box1 = useRef(null);
+    let box2 = useRef(null);
+    let box3 = useRef(null);
+    let box4 = useRef(null);
+    let box5 = useRef(null);
+    let box6 = useRef(null);
+    let box7 = useRef(null);
+    let box8 = useRef(null);
+    let box9 = useRef(null);
+
+    let box_array =[box1,box2,box3, box4,box5,box6,box7,box8, box9]
+
     const toogle = (e, num) =>{
         if (lock){
             return 0;
@@ -66,26 +78,29 @@ const TicTac= () =>{
     const reset = ()=> {
         setLock(false);
         data = ["","","","","","","","",""];
-        titleRef.current.innerHTML = "Tic Tac Toe"
+        titleRef.current.innerHTML = "Tic Tac Toe";
+        box_array.map((e) => {
+            e.current.innerHTML= "";
+        })
     }
     return(
         <div className='container'>
             <h1 ref={titleRef}>Tic Tac Toe</h1>
             <div className='board'>
                 <div className='row1'>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 0)}}></div>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 1)}}></div>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 2)}}></div>
+                    <div className='boxes' ref={box1} onClick={(e)=>{toogle(e, 0)}}></div>
+                    <div className='boxes' ref={box2} onClick={(e)=>{toogle(e, 1)}}></div>
+                    <div className='boxes' ref={box3} onClick={(e)=>{toogle(e, 2)}}></div>
                 </div>
                 <div className='row2'>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 3)}}></div>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 4)}}></div>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 5)}}></div>
+                    <div className='boxes' ref={box4}onClick={(e)=>{toogle(e, 3)}}></div>
+                    <div className='boxes' ref={box5}onClick={(e)=>{toogle(e, 4)}}></div>
+                    <div className='boxes'ref={box6} onClick={(e)=>{toogle(e, 5)}}></div>
                 </div>
                 <div className='row3'>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 6)}}></div>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 7)}}></div>
-                    <div className='boxes' onClick={(e)=>{toogle(e, 8)}}></div>
+                    <div className='boxes' ref={box7} onClick={(e)=>{toogle(e, 6)}}></div>
+                    <div className='boxes' ref={box8} onClick={(e)=>{toogle(e, 7)}}></div>
+                    <div className='boxes' ref={box9} onClick={(e)=>{toogle(e, 8)}}></div>
                 </div>
             </div>
             <button className='btn' onClick={()=> {reset()}}>Refresh</button>
